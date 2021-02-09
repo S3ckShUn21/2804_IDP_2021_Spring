12f84ec29cd06e5e3f269ddc6ca7b257b59a1635
Cole Roof <ckroof@gmail.com>
2021-02-09 13:34:36 -0500

    Reorganied Arduino Code

---
+++ ArduinoCode/BluetoothTest/BluetoothTest.ino
@@ -0,0 +1,31 @@
+#include<SoftwareSerial.h>
+
+int answer = 0;
+SoftwareSerial bluetooth(2, 3);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  Serial.println("Would you like to send data? Enter '1' for yes or else for no.: ");
+  Serial.println("\n");
+while (Serial.available() == 0) {
+    // Wait for input
+  }
+  answer = Serial.parseInt();
+
+  if (answer == 1)
+  {
+    // send data
+    bluetooth.println("Data recieved. ");
+    Serial.println("Data sent. ");
+    answer = 0;
+  }
+  
+}
