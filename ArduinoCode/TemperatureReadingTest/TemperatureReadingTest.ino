db5a3f14812b8b1ccf5747687d0a1400d5ad9c58
Cole Roof <ckroof@gmail.com>
2021-03-16 13:09:24 -0400

    Untested Temperature Reading Test Code

---
+++ ArduinoCode/TemperatureReadingTest/TemperatureReadingTest.ino
@@ -0,0 +1,34 @@
+#define TEMP_READ_PIN A0
+
+#define TEMP_ZERO_DEGREES_OFFSET  500  // 500mV
+#define TEMP_MV_PER_C             10   // 10mV / *C
+
+char outputStr[32];
+
+void setup() {
+  Serial.being(9600);
+  pinMode(TEMP_READ_PIN, INPUT);
+}
+
+void loop() {
+
+  // gather the data
+  int reading = analogRead(TEMP_READ_PIN);
+  float voltage_mV = reading * (5f/1024f);       // (5v / 1024setps) is the multiplier per step
+  int temp = translateTemperature( voltage_mV );
+
+  // format the output
+  sprintf( outputStr, "%04d, %4.2f, %d", reading, voltage, temp );
+  Serial.println( outputStr );
+
+  delay(100); // data collection @ 10Hz so you can actually read the data
+  
+}
+
+// Based around the MCP 9700E
+// Takes a voltage in milivolts and turn it into a temperature reading in *C
+int translateTemperature( float voltage ) {
+  float deltaV = voltage - (float)TEMP_ZERO_DEGREES_OFFSET; // get the voltage to the zero *C point
+  int degreesC = round( deltaV / (float)TEMP_MV_PER_C );    // based on the slope, find the *C
+  return degreesC;
+}
